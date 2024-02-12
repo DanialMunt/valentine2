c6c2ed24130050f48472c0f1ada1acff21d3c75d
Danial Muntinov <100083419+DanialMunt@users.noreply.github.com>
2024-02-13 01:51:29 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+//const mongoose = require('mongoose');
 
 
-app.use(express.static(path.join(__dirname, 'simp')));
+
+ 
+
+app.use(express.static(path.join(__dirname, 'valentine.github.io-main')));
 
 app.listen(3000, () => {
     console.log("App working BLYa");
